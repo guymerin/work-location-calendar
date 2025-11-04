03b921b4b2357b1cceb14181422bb776fc0fd2d0
Guy Merin <guy_merin@outlook.com>
2025-11-03 19:14:04 -0800

    add config.js file to github

---
+++ config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+// REPLACE THESE VALUES WITH YOUR OWN FIREBASE CONFIGURATION
+// Get your config from: https://console.firebase.google.com/
+
+
+window.firebaseConfig = {
+    apiKey: "AIzaSyCrD-e5rZczj98WfTn1H3mRS7klSWZz9j0",
+    authDomain: "rto-db.firebaseapp.com",
+    projectId: "rto-db",
+    storageBucket: "rto-db.appspot.com",
+    messagingSenderId: "361275779673",
+    appId: "1:361275779673:web:d65a7126821f57e54a1f48"
+};
